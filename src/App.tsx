41a22e598b379ee7f04c4fdb91330c2ab70201c3
MLGalusha <masonlgalusha@gmail.com>
2025-02-17 16:05:26 -0500

    added local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ItemList from "./components/ItemList";
 import "./components/styles/App.css";
 import { ItemType } from "./types";
 
 export default function App() {
-  const [itemList, setItemList] = useState<ItemType[]>([]);
+  const [itemList, setItemList] = useState<ItemType[]>(() => {
+    const storedItems = localStorage.getItem("groceryItems");
+    return storedItems ? JSON.parse(storedItems) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("groceryItems", JSON.stringify(itemList));
+  }, [itemList]);
 
   function addItem(item: ItemType) {
     setItemList([...itemList, item]);
